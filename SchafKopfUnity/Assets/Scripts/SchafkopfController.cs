52de2920e609981e03d4e622b9870e3115854254
johannessiedersberger <johannes.siedersberger@gmail.com>
2020-03-24 12:03:37 +0100

    Set CardValue To Sprite

--- SchafKopfUnity/Assets/Scripts/SchafkopfController.cs
+++ SchafKopfUnity/Assets/Scripts/SchafkopfController.cs
@@ -1,17 +1,29 @@
 ﻿using System.Runtime.InteropServices;
 using UnityEngine;
 using Schafkopf;
+using System;
+using System.Linq;
+using System.Collections.Generic;
 
 public class SchafkopfController : MonoBehaviour
 {
   public Sprite[] CardFaces;
-
+  private Dictionary<CardValues, Sprite> ValueToSprite = new Dictionary<CardValues, Sprite>();
 
   // Start is called before the first frame update
   void Start()
   {
     SchafkopfGame game = new SchafkopfGame();
-    
+    SetCardValueToSprite(CardFaces, ValueToSprite);
+
+  }
+
+  private static void SetCardValueToSprite(Sprite[] cardFaces, Dictionary<CardValues, Sprite> dictionary)
+  {
+    var cardValues = Enum.GetValues(typeof(CardValues)).Cast<CardValues>().ToArray();
+    for (int i = 0; i < cardFaces.Length; i++)
+      dictionary.Add(cardValues[i], cardFaces[i]);
+
   }
 
   // Update is called once per frame
