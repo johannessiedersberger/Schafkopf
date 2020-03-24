a1332089463540a66357efdc463fd02677bff5ca
johannessiedersberger <johannes.siedersberger@gmail.com>
2020-03-24 11:21:42 +0100

    Add dll, Add Sprites to Slots

---
+++ SchafKopfUnity/Assets/Scripts/SchafkopfController.cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime.InteropServices;
+using UnityEngine;
+using Schafkopf;
+
+public class SchafkopfController : MonoBehaviour
+{
+  public Sprite[] CardFaces;
+
+
+  // Start is called before the first frame update
+  void Start()
+  {
+    SchafkopfGame game = new SchafkopfGame();
+    
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+
+  }
+
+}
